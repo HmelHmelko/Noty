1cf18cd99c4855edc5fe9bd665144005ce18ca37
killtoyz <48437486+killtoyz@users.noreply.github.com>
2023-10-21 14:36:35 +0300

    MenuItem-Open
    Open text file -> text to rich text box

--- Noty/MainWindow.xaml.cs
+++ Noty/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,65 @@ namespace Noty
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool match = true;
+        string filePath = string.Empty;
+        string titleName = string.Empty;
+        string documentContent = string.Empty;
+
+        string DocumentContent 
+        { 
+            get { return documentContent; } 
+            set { documentContent = value; } 
+        }
+
+        string TitleName
+        {
+            get { return titleName; }
+            set { titleName = value; }
+        }
+
+        string FilePath
+        {
+            get { return filePath; }
+            set { filePath = value; }
+        }
+
+        bool Match
+        {
+            get { return match; }
+            set { match = value; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenMenuItemClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog();
+        }
+
+        private void OpenFileDialog()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            
+            if(ofd.FileName != null)
+            {
+                //Path to document
+                FilePath = ofd.FileName;
+
+                //Document name
+                TitleName = ofd.SafeFileName;
+
+                //if the document matches the textbox
+                Match = true;
+
+                StreamReader fileReader = new StreamReader(FilePath);
+                DocumentContent = fileReader.ReadToEnd();;
+                textArea.AppendText(documentContent);
+            }
         }
     }
 }
