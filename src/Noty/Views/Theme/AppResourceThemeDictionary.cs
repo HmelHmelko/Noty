b2ba545088a89b7930407fe14901dfd08919355c
killtoyz <48437486+killtoyz@users.noreply.github.com>
2023-11-02 02:30:10 +0300

    New App theme (not implemented)

---
+++ src/Noty/Views/Theme/AppResourceThemeDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+
+namespace Noty.Views.Theme
+{
+    public class AppResourceThemeDictionary : ResourceDictionary
+    {
+        private Uri darkGray;
+        private Uri darkBlue;
+
+        public Uri DarkGraySource
+        {
+            get { return darkGray; }
+            set
+            {
+                darkGray = value;
+                UpdateSource();
+            }
+        }
+        public Uri DarkBlueSource
+        {
+            get { return darkBlue; }
+            set
+            {
+                darkBlue = value;
+                UpdateSource();
+            }
+        }
+
+        private void UpdateSource()
+        {
+            var val = App.Skin == Skin.Red ? DarkGraySource : DarkBlueSource;
+            if (val != null && base.Source != val)
+                base.Source = val;
+        }
+    }
+}
