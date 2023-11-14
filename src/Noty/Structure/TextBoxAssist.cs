7083becaa90a151726937394af5f7d24d23259c2
killtoyz <killtoyzmail@gmail.com>
2023-11-15 00:41:07 +0300

    feat: добавлен класс TextBoxAssist для контроля доп. свойств textbox + тестовая команда CopyText

---
+++ src/Noty/Structure/TextBoxAssist.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Noty.Structure
+{
+    public static class TextBoxAssist
+    {
+        // This strange default value is on purpose it makes the initialization problem very unlikely.
+        // If the default value matches the default value of the property in the ViewModel,
+        // the propertyChangedCallback of the FrameworkPropertyMetadata is initially not called
+        // and if the property in the ViewModel is not changed it will never be called.
+        private const string SelectedTextPropertyDefault = "pxh3949%lm/kkoiladasvvbnnn///$$$$21235";
+
+        public static string GetSelectedText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(SelectedTextProperty);
+        }
+
+        public static void SetSelectedText(DependencyObject obj, string value)
+        {
+            obj.SetValue(SelectedTextProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedTextProperty =
+            DependencyProperty.RegisterAttached(
+                "SelectedText",
+                typeof(string),
+                typeof(TextBoxAssist),
+                new FrameworkPropertyMetadata(
+                    SelectedTextPropertyDefault,
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                    SelectedTextChanged));
+
+        private static void SelectedTextChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            if (dependencyObject is not TextBox textBox)
+            {
+                return;
+            }
+
+            var oldValue = eventArgs.OldValue as string;
+            var newValue = eventArgs.NewValue as string;
+
+            if (oldValue == SelectedTextPropertyDefault && newValue != SelectedTextPropertyDefault)
+            {
+                textBox.SelectionChanged += SelectionChangedForSelectedText;
+            }
+            else if (oldValue != SelectedTextPropertyDefault && newValue == SelectedTextPropertyDefault)
+            {
+                textBox.SelectionChanged -= SelectionChangedForSelectedText;
+            }
+
+            if (newValue is not null && newValue != textBox.SelectedText)
+            {
+                textBox.SelectedText = newValue;
+            }
+        }
+
+        private static void SelectionChangedForSelectedText(object sender, RoutedEventArgs eventArgs)
+        {
+            if (sender is TextBox textBox)
+            {
+                SetSelectedText(textBox, textBox.SelectedText);
+            }
+        }
+    }
+}
--- src/Noty/ViewModels/EditorViewModel.cs
+++ src/Noty/ViewModels/EditorViewModel.cs
@@ -1,4 +1,7 @@
-﻿using Noty.Models;
+﻿using Noty.Commands;
+using Noty.Models;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Noty.ViewModels
 {
@@ -7,7 +10,30 @@ namespace Noty.ViewModels
         public DocumentModel Document { get; private set; }
         public FormatModel Format { get; private set; }
 
+        #region TextControlCommands
+        #region testCommand
+        public ICommand testCommand { get; }
+        private bool CanTestCommandExecute(object p) => true;
+        private void OnTestCommandExecuted(object p)
+        {
+            var hello = SelectedText;
+        }
+        #endregion
+
+        public ICommand CopyTextCommand { get; }
+        private bool CanCopyTextCommandExecute(object p) => true;
+        private void OnCopyTextCommandExecuted(object p)
+        {
+            Clipboard.SetText(SelectedText);
+        }
+        #endregion
+
         #region Properties
+        public string SelectedText 
+        { 
+            get; 
+            set; 
+        }
         public string? TextContent
         {
             get => Document.TextContent;
@@ -38,6 +64,8 @@ namespace Noty.ViewModels
         {
             Document = document;
             Format = new FormatModel();
+            testCommand = new DelegateCommand(OnTestCommandExecuted, CanTestCommandExecute);
+            CopyTextCommand = new DelegateCommand(OnCopyTextCommandExecuted, CanCopyTextCommandExecute);
         }
     }
 }
